b5f08b98dff7bb58e471cbc5734448320d2ed2ae
Natale Vinto <ebballon@gmail.com>
2016-05-01 19:33:21 +0200

    Fix trusty check

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -17,14 +17,15 @@ class crossbar::repo {
     }
   } elsif $::operatingsystem == "Ubuntu" {
     case $osver[0] {
-      '14.04' : { $baseurl = 'http://package.crossbar.io/centos/7/' }
+      '14.04' : { $release = 'trusty' }
       default : { fail('Unsupported version of Ubuntu') }
     }
     include apt
+
     apt::source { 'crossbar':
       comment  => 'Crossbar.iot official binary packages',
       location => 'http://package.crossbar.io/ubuntu',
-      release  => 'trusty',
+      release  => $release,
       repos    => 'main',
       key      => {
         'id'     => '665415B518DA028EFB57E9BF5FC6281FD58C6920',
